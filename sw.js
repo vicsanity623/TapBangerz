364a4bda304b15f654fbe4308b57543ab4354217
Vic <125237471+vicsanity623@users.noreply.github.com>
2026-02-14 02:03:45 -0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'tapB-v1.0.0';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html'
+];
+
+// Install Event: Cache files
+self.addEventListener('install', (event) => {
+  // FORCE UPDATE: Activate new service worker immediately
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Activate Event: Clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log('Clearing old cache:', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  // FORCE UPDATE: Take control of all open clients immediately
+  return self.clients.claim();
+});
+
+// Fetch Event: Serve from cache if available, otherwise fetch from network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
